1028c3236b4145b48aea995a0902e64d4624d80f
aujezus <augispay@gmail.com>
2023-11-04 16:14:17 +0200

    Add blog header

--- src/features/blog/Blog.jsx
+++ src/features/blog/Blog.jsx
@@ -1,11 +1,18 @@
 import { BlockNoteView, useBlockNote } from "@blocknote/react";
 import TextareaAutosize from "react-textarea-autosize";
 import "@blocknote/core/style.css";
-import { BiCalendarHeart, BiCategoryAlt, BiTagAlt } from "react-icons/bi";
+import {
+  BiCalendarHeart,
+  BiCategoryAlt,
+  BiChevronDown,
+  BiTagAlt,
+  BiX,
+} from "react-icons/bi";
 import * as dayjs from "dayjs";
 import { IconContext } from "react-icons";
 import { useState } from "react";
 import { Listbox } from "@headlessui/react";
+import Button from "../../ui/Button";
 
 function Blog() {
   async function printContent() {
@@ -22,6 +29,24 @@ function Blog() {
   ];
 
   const [selectedCategory, setSelectedCategory] = useState(categories[0]);
+  const [tags, setTags] = useState([]);
+  const [tag, setTag] = useState("");
+
+  function addTag(e) {
+    e.preventDefault();
+
+    if (tags.includes(tag)) return;
+
+    setTags((tags) => [...tags, tag.split(" ").join("-")]);
+    setTag("");
+  }
+
+  function removeTag(e) {
+    e.preventDefault();
+
+    console.log(e.target.innerText);
+    setTags((tags) => tags.filter((t) => t !== e.target.textContent));
+  }
 
   return (
     <div className="px-2 py-6">
@@ -31,7 +56,7 @@ function Blog() {
           placeholder="Your best blog title ever..."
         ></TextareaAutosize>
 
-        <div className="flex flex-col gap-4 text-3xl">
+        <div className="mb-10 flex flex-col gap-4 text-2xl">
           <IconContext.Provider value={{ className: "text-violet-500" }}>
             <div className="flex items-center gap-3">
               <BiCalendarHeart />
@@ -44,12 +69,14 @@ function Blog() {
                   value={selectedCategory}
                   onChange={setSelectedCategory}
                 >
-                  <Listbox.Button>{selectedCategory.name}</Listbox.Button>
+                  <Listbox.Button className="flex items-center gap-3">
+                    {selectedCategory.name} <BiChevronDown />
+                  </Listbox.Button>
                   <Listbox.Options className="absolute top-full flex flex-col gap-4 bg-neutral-700 p-4">
                     {categories.map((category) => (
                       <Listbox.Option
                         key={category.id}
-                        value={category.id}
+                        value={category}
                         className="border-b-2 border-violet-500"
                       >
                         {category.name}
@@ -60,15 +87,36 @@ function Blog() {
               </div>
             </div>
             <div className="flex items-center gap-3">
-              <BiTagAlt />
-              Web Dev
+              <BiTagAlt className="flex-shrink-0" />
+              <input
+                value={tag}
+                onChange={(e) => setTag(e.target.value)}
+                type="text"
+                className="w-full bg-transparent outline-none ring-neutral-500 placeholder:text-neutral-500 focus:ring-1"
+                placeholder="New Tag..."
+              />
+              <Button click={addTag} size="sm">
+                Add tag
+              </Button>
             </div>
           </IconContext.Provider>
         </div>
+
+        <div className="mb-10 flex flex-wrap justify-center gap-6 text-sm">
+          {tags.map((t) => (
+            <span
+              key={t}
+              onClick={removeTag}
+              className="flex items-center gap-2 rounded-lg bg-neutral-800 px-2 py-1"
+            >
+              {t}
+              <BiX className="text-red-500" />
+            </span>
+          ))}
+        </div>
       </form>
 
-      {/* <BlockNoteView editor={editor} />
-      <button onClick={printContent}>Print the shit</button> */}
+      <BlockNoteView editor={editor} />
     </div>
   );
 }
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,13 +1,23 @@
-function Button({ children, type = "secondary", size, link = "" }) {
+function Button({
+  children,
+  type = "secondary",
+  size = "lg",
+  link = "",
+  click = {},
+}) {
   const baseClasses =
-    "flex items-center gap-1 border-2  px-2 py-1 text-lg transition-all hover:-translate-x-2 hover:-translate-y-2 hover:shadow-[0.5rem_0.5rem_0px]  active:translate-x-0 active:translate-y-0 active:shadow-none active:duration-75";
+    "flex items-center gap-1 border-2 flex-shrink-0 px-2 py-1 transition-all hover:-translate-x-2 hover:-translate-y-2 hover:shadow-[0.5rem_0.5rem_0px]  active:translate-x-0 active:translate-y-0 active:shadow-none active:duration-75";
 
+  const sizes = {
+    sm: "text-sm",
+    lg: "text-lg",
+  };
   const types = {
     primary: "border-violet-500 text-violet-500 hover:shadow-violet-500",
     secondary: "border-neutral-400 text-neutral-400 hover:shadow-neutral-400",
   };
 
-  const completeClasses = `${baseClasses} ${types[type]}`;
+  const completeClasses = `${baseClasses} ${types[type]} ${sizes[size]}`;
 
   if (link)
     return (
@@ -16,7 +26,11 @@ function Button({ children, type = "secondary", size, link = "" }) {
       </a>
     );
 
-  return <button className={completeClasses}>{children}</button>;
+  return (
+    <button onClick={click} className={completeClasses}>
+      {children}
+    </button>
+  );
 }
 
 export default Button;
